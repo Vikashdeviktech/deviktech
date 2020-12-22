8e3b80eefabaf4cf1a7da871f79b4d4e450b71cc
vikashdeviktech <dubey.amansus@gmail.com>
2020-12-21 19:43:02 -0800

    Updates

---
+++ precache-manifest.e162e03d93007588720879d08ab0c1c8.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = [
+  {
+    "revision": "60433c3e6095fda5ac3a",
+    "url": "/static/css/main.76994bb6.chunk.css"
+  },
+  {
+    "revision": "60433c3e6095fda5ac3a",
+    "url": "/static/js/main.c6fd5b70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba22d2f4061d857376fa",
+    "url": "/static/css/2.0e632296.chunk.css"
+  },
+  {
+    "revision": "ba22d2f4061d857376fa",
+    "url": "/static/js/2.28b91d46.chunk.js"
+  },
+  {
+    "revision": "1148c3f8e2967ac6bec44c39b0440b18",
+    "url": "/static/media/About.1148c3f8.png"
+  },
+  {
+    "revision": "681b62b9dbd167d97f87d6f8b3c0316f",
+    "url": "/static/media/collegeImage.681b62b9.png"
+  },
+  {
+    "revision": "eae4c977586f8a17ae64bb5ecaca1362",
+    "url": "/static/media/python_wbg.eae4c977.png"
+  },
+  {
+    "revision": "cd6a5910046dff8b7d41e1edee7bda58",
+    "url": "/static/media/PythonSchool.cd6a5910.png"
+  },
+  {
+    "revision": "d7b8381e5c5509a7f98d0f9656bbcbf8",
+    "url": "/static/media/C++wbg.d7b8381e.png"
+  },
+  {
+    "revision": "729baeca1bfc601466e9451c0530640b",
+    "url": "/static/media/java_wbg.729baeca.png"
+  },
+  {
+    "revision": "4d6ddf9482f5bfd3befedbd2251fdb5d",
+    "url": "/static/media/javascript_wbg.4d6ddf94.png"
+  },
+  {
+    "revision": "f1f74837bac4144841c97465c4b2b229",
+    "url": "/static/media/node_wbg.f1f74837.png"
+  },
+  {
+    "revision": "9667bb13c37a10b4cb3eccb47e816a3c",
+    "url": "/static/media/mongo_wbg.9667bb13.png"
+  },
+  {
+    "revision": "213c459a0b76be5eca1b1803a3aa2154",
+    "url": "/static/media/ScratchLogo.213c459a.png"
+  },
+  {
+    "revision": "532aa7194e1cf8fc1c423519e471ca58",
+    "url": "/static/media/MitLogo.532aa719.png"
+  },
+  {
+    "revision": "35db59c87e9e6f0e0ecfb195685f9ba3",
+    "url": "/static/media/MERNLogo.35db59c8.png"
+  },
+  {
+    "revision": "355c2963a8653b5509467fc7f4c1f2b6",
+    "url": "/static/media/C.355c2963.png"
+  },
+  {
+    "revision": "63ea07ca86dee6c4142816956e8c4395",
+    "url": "/static/media/html_wbg.63ea07ca.png"
+  },
+  {
+    "revision": "efba608357aaa885a757521619bd4447",
+    "url": "/static/media/AnimationLogo.efba6083.png"
+  },
+  {
+    "revision": "fa8fd2d88ab058eb15d36e54a529c286",
+    "url": "/static/media/GirlOnMainArea.fa8fd2d8.png"
+  },
+  {
+    "revision": "8b87da30e1448711546830ea501511c7",
+    "url": "/static/media/RealTimeProject.8b87da30.png"
+  },
+  {
+    "revision": "879a2d1330dec2c4eba572e883912867",
+    "url": "/static/media/SchoolSupport.879a2d13.png"
+  },
+  {
+    "revision": "999eda2ba1e01990e691a51336ab618d",
+    "url": "/static/media/SchoolCurriculum.999eda2b.png"
+  },
+  {
+    "revision": "94bbf7d31cf19a92ba23b227f5daeaaa",
+    "url": "/static/media/Certification.94bbf7d3.png"
+  },
+  {
+    "revision": "44f089ce9730759e22fcc5d9269bd189",
+    "url": "/static/media/SchoolDemo.44f089ce.png"
+  },
+  {
+    "revision": "c23e91707435b910c14a543dbec683f4",
+    "url": "/static/media/UnityLogo.c23e9170.png"
+  },
+  {
+    "revision": "583bdadcbc6bf6d76145aa93adbcb813",
+    "url": "/static/media/WebDesign.583bdadc.png"
+  },
+  {
+    "revision": "c6a00dd985cbfcb24138fc82d45945ea",
+    "url": "/static/media/BgMainArea.c6a00dd9.png"
+  },
+  {
+    "revision": "0f255440cfc694f8e3820e6d5d74a6be",
+    "url": "/static/media/EthicalHacking.0f255440.png"
+  },
+  {
+    "revision": "d5ffc78db1f67304a84c68d29e7fff33",
+    "url": "/static/media/collegeImage900.d5ffc78d.png"
+  },
+  {
+    "revision": "7cc0e77fc7042d152dad743170e07b25",
+    "url": "/static/media/SchoolGame.7cc0e77f.png"
+  },
+  {
+    "revision": "1a87cfd36eac1201c7290c013ff856f4",
+    "url": "/static/media/SchoolWebDevelopment.1a87cfd3.png"
+  },
+  {
+    "revision": "67ca8b441c2cd0f820b0b3139fc8d119",
+    "url": "/static/media/SchoolMobileDevelopment.67ca8b44.png"
+  },
+  {
+    "revision": "48d2cfb1b67d7547327c55388fd0cf38",
+    "url": "/static/media/Assignment.48d2cfb1.png"
+  },
+  {
+    "revision": "53c03381e47f8afc89540a511381c3c1",
+    "url": "/static/media/Logo.White.53c03381.png"
+  },
+  {
+    "revision": "44641df3e3b457c21db3c87796b77a8a",
+    "url": "/static/media/CollegeInternship.44641df3.png"
+  },
+  {
+    "revision": "996f6a2bb0ec3c728d330a01d49ca5f9",
+    "url": "/static/media/MernCourse.996f6a2b.png"
+  },
+  {
+    "revision": "99b8d9d1796d96c921deef4c1400e7b5",
+    "url": "/static/media/HappyImage.99b8d9d1.png"
+  },
+  {
+    "revision": "48ba2fb728ae5347c90f051046b87cb5",
+    "url": "/static/media/HappyImage2.48ba2fb7.png"
+  },
+  {
+    "revision": "97593dd54a851613d4b64833c4ee1991",
+    "url": "/static/media/HappyImage3.97593dd5.png"
+  },
+  {
+    "revision": "148bd07a6091be7e4d3ef0ffd5050633",
+    "url": "/static/media/BootstrapLogo.148bd07a.png"
+  },
+  {
+    "revision": "49b15fe943e73d2856d81b53cdc56053",
+    "url": "/static/media/CSSLogo.49b15fe9.png"
+  },
+  {
+    "revision": "e263592a0788c585ef222369bf026c88",
+    "url": "/static/media/RealLifeIcon.e263592a.png"
+  },
+  {
+    "revision": "c0ddfefdacd2464b70a24a18eebe7e6d",
+    "url": "/static/media/24x7Icon.c0ddfefd.png"
+  },
+  {
+    "revision": "c31f830fb8122e5fd2878672c5399fc4",
+    "url": "/static/media/blog1.c31f830f.png"
+  },
+  {
+    "revision": "9378f697fdc34141dbdccf549a36decd",
+    "url": "/static/media/blog2.9378f697.png"
+  },
+  {
+    "revision": "e381d6dc063c412f16ede8a82047aab8",
+    "url": "/static/media/blog3.e381d6dc.png"
+  },
+  {
+    "revision": "8ecbdb5817085bbc5ee184ac27cf1ba0",
+    "url": "/static/media/blog4.8ecbdb58.png"
+  },
+  {
+    "revision": "b640f5cbbd648dc9acf8b6c9e5b40613",
+    "url": "/static/media/blog5.b640f5cb.png"
+  },
+  {
+    "revision": "40a473fc0746d2a883b727b0f9ee2db7",
+    "url": "/static/media/blog6.40a473fc.png"
+  },
+  {
+    "revision": "937e81dff91a0de7c457082fb210ee88",
+    "url": "/static/media/googleMap.937e81df.png"
+  },
+  {
+    "revision": "2b8a561a5865af6746e78b4e0d556abe",
+    "url": "/static/media/FOUNDATION.2b8a561a.png"
+  },
+  {
+    "revision": "9aa39a9e2c947a2086be0ac36acf069f",
+    "url": "/static/media/DEVELOPMENT.9aa39a9e.png"
+  },
+  {
+    "revision": "1e9730d5b79f65855a75b2e6971635d8",
+    "url": "/static/media/ADVANCE.1e9730d5.png"
+  },
+  {
+    "revision": "da33ea6ced27d99687a2e2b5e43235e8",
+    "url": "/static/media/1.da33ea6c.png"
+  },
+  {
+    "revision": "a93da65e9d0cc97d375bbab0fa9532d1",
+    "url": "/static/media/2.a93da65e.png"
+  },
+  {
+    "revision": "170ecc1ca79670f9b8fe12ba9d076542",
+    "url": "/static/media/3.170ecc1c.png"
+  },
+  {
+    "revision": "4dd4e6d5555f922e4fbf2b7955cb364b",
+    "url": "/static/media/4.4dd4e6d5.png"
+  },
+  {
+    "revision": "2a59979532485bb4ba0d073b90438839",
+    "url": "/static/media/5.2a599795.png"
+  },
+  {
+    "revision": "e116f788c15a2840ab2dd5e681e2e9ce",
+    "url": "/static/media/6.e116f788.png"
+  },
+  {
+    "revision": "0298edb37c42d61d17bfcbafd380fba5",
+    "url": "/static/media/CollegeHacking.0298edb3.png"
+  },
+  {
+    "revision": "8bcf8f0610bc975b9ab82f8ca33f262f",
+    "url": "/static/media/CollegeData.8bcf8f06.png"
+  },
+  {
+    "revision": "31726c07c17afdbc57e0b2926c13d807",
+    "url": "/static/media/MachineLogo.31726c07.png"
+  },
+  {
+    "revision": "abe17defcf1fb82e2b002fdf585ccfd0",
+    "url": "/static/media/AppLogo.abe17def.png"
+  },
+  {
+    "revision": "09c0a511adc5b52e4114622a95041a64",
+    "url": "/static/media/DataLogo.09c0a511.png"
+  },
+  {
+    "revision": "8279dab322e08ab61bade129189baa76",
+    "url": "/static/media/About34.8279dab3.png"
+  },
+  {
+    "revision": "74852f17d5b8cf43b5136f5eabe888eb",
+    "url": "/static/media/About4.74852f17.png"
+  },
+  {
+    "revision": "02491b0acc84a0692728f341d4a3468a",
+    "url": "/static/media/About3.02491b0a.png"
+  },
+  {
+    "revision": "4ddb7c57f5670a2a6a9115f02b4585cc",
+    "url": "/static/media/Sliders1.4ddb7c57.jpg"
+  },
+  {
+    "revision": "b4e61111e0f1b8d1f77f86980fc039ac",
+    "url": "/static/media/Sliders2.b4e61111.jpg"
+  },
+  {
+    "revision": "d366208907a23170857d932240b1443c",
+    "url": "/static/media/Sliders3.d3662089.jpg"
+  },
+  {
+    "revision": "7959bc905f48d61fc118fd32a4fe2b7e",
+    "url": "/static/media/SchoolAnimation.7959bc90.png"
+  },
+  {
+    "revision": "d88aa9e8831e1d5fba702c2f80be5bdd",
+    "url": "/static/media/School3D.d88aa9e8.png"
+  },
+  {
+    "revision": "f9605bc7b3eb037c3bca4fe9c5b8611d",
+    "url": "/static/media/GithubLink.f9605bc7.png"
+  },
+  {
+    "revision": "658712dda3a3214898264e062f0e416c",
+    "url": "/static/media/PythonKidsCourse.658712dd.png"
+  },
+  {
+    "revision": "c58a73b25cfa26d2684c03a36d89f0d4",
+    "url": "/static/media/MachineLearningCourse.c58a73b2.png"
+  },
+  {
+    "revision": "4f0621d5bf920aa26b543abb6e8379b7",
+    "url": "/static/media/DataStructureCourse.4f0621d5.png"
+  },
+  {
+    "revision": "0871112541fdeb7ce899034cf0a746d4",
+    "url": "/static/media/EthicalHackingCourse.08711125.png"
+  },
+  {
+    "revision": "f6f9950124e0e982fa77af88a78c7a6c",
+    "url": "/static/media/Sliders4.f6f99501.jpg"
+  },
+  {
+    "revision": "8d9b63b2bb247cea81b938f8c08c65a8",
+    "url": "/index.html"
+  }
+];
